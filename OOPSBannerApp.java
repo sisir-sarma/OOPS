5205c41ed291c8bb545cc9305d9abb9bdc52aeea
sisir-sarma <sisir2007.turlapati@gmail.com>
2026-02-23 01:47:31 +0530

    added banner feature

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,15 @@
 public class OOPSBannerApp{
-   public static void main(String[] args) {
-        
-        System.out.println("    ***    " + "   ***    " + " ******    " + "   ***** ");
-        System.out.println("  **   **  " + " **   **  " + " **    **  " + " **      ");
-        System.out.println(" **     ** " + "**     ** " + " **     ** " + "**       ");
-	    System.out.println(" **     ** " + "**     ** " + " **    **  " + " **      ");
-        System.out.println(" **     ** " + "**     ** " + " ******    " + "   ***   ");
-        System.out.println(" **     ** " + "**     ** " + " **        " + "      ** ");
-	    System.out.println(" **     ** " + "**     ** " + " **        " + "       **");
-        System.out.println("  **   **  " + " **   **  " + " **        " + "      ** ");
-        System.out.println("    ***    " + "   ***    " + " **        " + " *****   ");
-    }
+  public static void main(String[] args) {
+
+    System.out.println(String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** "));
+    System.out.println(String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      "));
+    System.out.println(String.join(" ", "**     **", "**     **", "**     **", "**       "));
+    System.out.println(String.join(" ", "**     **", "**     **", "**    ** ", " **      "));
+    System.out.println(String.join(" ", "**     **", "**     **", "******   ", "   ***   "));
+    System.out.println(String.join(" ", "**     **", "**     **", "**       ", "      ** "));
+    System.out.println(String.join(" ", "**     **", "**     **", "**       ", "       **"));
+    System.out.println(String.join(" ", " **   ** ", " **   ** ", "**       ", "      ** "));
+    System.out.println(String.join(" ", "   ***   ", "   ***   ", "**       ", " *****   "));
+
+  }
 }
